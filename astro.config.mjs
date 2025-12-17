5949dffdc889baeed9f989439c494577da111f89
southvanfc <southvanfc@gmail.com>
2025-12-16 17:40:40 -0800

    change to vercel, make many changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  adapter: vercel()
 });
