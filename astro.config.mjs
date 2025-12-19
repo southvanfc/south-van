ae8c2ca457b6999db024c020d5fad291822a0b32
southvanfc <southvanfc@gmail.com>
2025-12-19 15:40:30 -0800

    fix form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
-
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [icon()],
-  adapter: vercel(),
-  imageService: true,
+   adapter: vercel({ imageService: true }),
 });
